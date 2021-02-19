4542c191d033e696ba62bec26d04dcaba7d32c8a
rracheev <rracheev@users.noreply.github.com>
2021-02-19 16:07:59 +0300

    secret_third

--- secret_app/app.js
+++ secret_app/app.js
@@ -5,36 +5,42 @@ const JSONParser = bodyParser.json();
 const fs = require('fs');
 const { subtle } = require('crypto').webcrypto;
 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const {nanoid} = require("nanoid");
 const app = express();
 
 const dir="/resource";
 
+const textSchema = new Schema({
+    _id: {
+      type: String,
+      default: () => nanoid(6)
+    },
+    password: String,
+    text: String
+  });
+const Text = mongoose.model("Text", textSchema);
+const text1 = new Text({
+    password: "qwerrty",
+    text: "XNJJSD"
+});
+	
+mongoose.connect("mongodb://localhost:27017/newdb", {useUnifiedTopology: true, useNewUrlParser: true});
+
+
 app.use(express.static(__dirname+dir+"/js"));
 app.use(express.static(__dirname+dir+"/css"));
 
- 
-async function generateKey(length = 256) {
-    const key = await subtle.generateKey(
-        {
-          name: "RSA-OAEP",
-          modulusLength: 512,
-          publicExponent: new Uint8Array([1, 0, 1]),
-          hash: "SHA-256"
-        },
-        true,
-        ["encrypt", "decrypt"]
-      );
-    return key;
-  }
-
-  console.log(generateKey())
 app.get("/", function(req, res){
    res.sendFile(__dirname+dir+"/html/index.html");
 });
 app.post("/send", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    console.log(request.body.pass1);
+    let new_text=new Text(request.body);
+    new_text.save()
 });
 app.post("/getpass",JSONParser, function (req,res) {
     res.send(JSON.stringify({
